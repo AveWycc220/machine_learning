f5dea923ce239918934004054662a16f8e52be39
AveWycc220 <mik16748@gmail.com>
2020-11-19 20:44:50 +0300

    feat(4_task)

--- 4_task/fourth_task.py
+++ 4_task/fourth_task.py
@@ -19,4 +19,19 @@ plt.xlabel('Date')
 plt.ylabel('Volume')
 value_list[0:int(len(value_list)/2)].plot(kind='bar', ax=axes[0])
 value_list[int(len(value_list)/2):len(value_list)].plot(kind='bar', ax=axes[1])
-plt.show()
+sum = csv['<CLOSE>'][0]
+average_list = pd.Series(dtype=int)
+count = 1
+for i in range(1, len(csv['<CLOSE>'])):
+    if csv['<DATE>'][i][6:] == csv['<DATE>'][i-1][6:]:
+        count += 1
+        sum += csv['<CLOSE>'][i]
+    else:
+        sum /= count
+        average_list = average_list.append(pd.Series([sum], [csv['<DATE>'][i-1][6:]]))
+        sum = csv['<CLOSE>'][i]
+        count = 1
+    if i == len(csv['<DATE>'])-1:
+        sum /= count
+        average_list = average_list.append(pd.Series([sum], [csv['<DATE>'][i-1][6:]]))
+print(average_list.values)
